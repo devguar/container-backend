3764597860981eab998a7361f9ddbf68d1cb7ad6
Lucas Guarnieri <lucasguarni@gmail.com>
2017-10-02 18:31:40 -0300

    Helper

---
+++ src/Util/ConvertObjectArrayHelper.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lucas
+ * Date: 02/10/17
+ * Time: 12:17
+ */
+
+namespace Devguar\OContainer\Util;
+
+
+class ConvertObjectArrayHelper
+{
+    /**
+     * Convert an array into a stdClass()
+     *
+     * @param   array   $array  The array we want to convert
+     *
+     * @return  object
+     */
+    public static function arrayToObject($array)
+    {
+        // First we convert the array to a json string
+        $json = json_encode($array);
+
+        // The we convert the json string to a stdClass()
+        $object = json_decode($json);
+
+        return $object;
+    }
+
+
+    /**
+     * Convert a object to an array
+     *
+     * @param   object  $object The object we want to convert
+     *
+     * @return  array
+     */
+    public static function objectToArray($object)
+    {
+        // First we convert the object into a json string
+        $json = json_encode($object);
+
+        // Then we convert the json string to an array
+        $array = json_decode($json, true);
+
+        return $array;
+    }
+}
