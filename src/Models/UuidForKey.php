5026c85456c35654ea05b7782d153d67000ad896
Lucas Guarnieri <lucasguarni@gmail.com>
2017-11-16 00:48:03 -0200

    Trait

---
+++ src/Models/UuidForKey.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lucas
+ * Date: 16/11/17
+ * Time: 00:33
+ */
+
+namespace Devguar\OContainer\Models;
+
+use Ramsey\Uuid\Uuid;
+
+trait UuidForKey
+{
+    /**
+     * Boot the Uuid trait for the model.
+     *
+     * @return void
+     */
+    public static function bootUuidForKey()
+    {
+        static::creating(function ($model) {
+            $model->incrementing = false;
+            $model->{$model->getKeyName()} = (string)Uuid::uuid4();
+        });
+    }
+}
